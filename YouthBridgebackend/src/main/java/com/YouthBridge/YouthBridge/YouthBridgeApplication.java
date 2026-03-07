7ad3c7ce4d7813aa00ea83d90fd2b15abdb76cf0
rudals5215 <rudals5215@naver.com>
2026-03-07 20:06:21 +0900

    first commit

---
+++ YouthBridgebackend/src/main/java/com/YouthBridge/YouthBridge/YouthBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.YouthBridge.YouthBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthBridgeApplication.class, args);
+	}
+
+}
---
+++ YouthBridgebackend/src/test/java/com/YouthBridge/YouthBridge/YouthBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YouthBridge.YouthBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
